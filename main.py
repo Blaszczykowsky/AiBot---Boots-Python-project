45f77a1d8865c51c962dda0eb7bbec18aa369002
Blaszczykowsky <daniel.blaszczykowski2002@gmail.com>
2026-08-12 04:36:21 +0200

    Initial commit for the AiAgent project from Boots

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
+def main():
+    messages = [
+        {
+            "role": "user",
+            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        }
+    ]
+
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+
+    if api_key == None:
+        raise RuntimeError("The api-key was not found")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+    response = client.chat.completions.create(model = 'openrouter/free', messages=messages)
+    if response.usage != None:
+        prompt_tokens = response.usage.prompt_tokens
+        completion_tokens = response.usage.completion_tokens
+    else:
+        raise RuntimeError("Something went wrong with response.usage")
+    print(f"User prompt: {messages}")
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {completion_tokens}")
+    print(f"Response:{response.choices[0].message.content}")
+
+if __name__ == "__main__":
+    main()
